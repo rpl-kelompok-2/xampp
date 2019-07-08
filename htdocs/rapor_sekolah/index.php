8880e1d8e87266ade7222326063950cf096198d1
Muhammad Eko Pujianto <mexop99@gmail.com>
2019-07-08 21:02:51 +0700

    Auto stash before merge of "login" and "origin/develop"

--- htdocs/rapor_sekolah/index.php
+++ htdocs/rapor_sekolah/index.php
@@ -1,40 +1,23 @@
+<?php
+// memulai session
+session_start();
+if (isset($_SESSION['login'])) { ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="style.css" />
-    <title>Sistem Informari Rapor Online</title>
+    <title>Dashboard</title>
 </head>
 <body>
-    <div class="center">
-        <center>
-        <h2>Login Wali Murid</h2>
-        <hr />
-        <form action="proses.php">
-            <table style="padding-top:25px">
-            <tr>
-                <td> Username </td>
-                <td> </td>
-                <td><input type="text" class="textForm" name="nis" /></td>
-            </tr>
-            <tr>
-                <td> Password </td>
-                <td> </td>
-                <td><input type="password" class="textForm" name="password" /></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td>
-                    <input type="button" value="Hapus" class="tombolForm" />
-                    <input type="submit" value="Login" name="login" class="tombolForm" />
-                </td>
-            </tr>
-            </table>
-        </form>
-        </center>
-    </div>
+    <center><h2>Sukses Login</h2></center>
 </body>
 </html>
+
+<?php
+} else {
+	header("location:login.php");
+}
+?>
--- htdocs/rapor_sekolah/proses.php
+++ htdocs/rapor_sekolah/proses.php
@@ -2,24 +2,16 @@
 // memulai session
 session_start();
 include 'koneksi.php'; 
-if (isset($_POST['login'])) {
+if (!isset($_SESSION['login'])) {
 	$nis	= $_POST['nis'];
 	$pswd	= $_POST['password'];
 
-	$query	= mysqli_query($connect, "SELECT * FROM user WHERE nis = '$nis' AND pswd = '$pswd'");
-	if (mysqli_num_rows($query) !==0) {
-		
-		$get	= mysqli_fetch_array($query);
-		$level	= $get['level_user'];
-		if ($level=="admin") {
-			$_SESSION['login'] = "admin";
-			header("location:halaman/admin.php");
-		} elseif ($level=='user_b') {
-			$_SESSION['login'] = "siswa";
-			header("location:halaman/siswa.php");
-		}
+	$query	= mysqli_query($connect, "SELECT * FROM user WHERE NIS = '$nis' AND pwd_user = '$pswd'");
+	if (mysqli_num_rows($query) !== 0 ) {
+		$_SESSION['login'] = "siswa";
+		header("location:index.php");
 	} else {
-		echo "Login gagal";
+		header("location:login.php");
 	}
 } else {
 	header("location:index.php");
